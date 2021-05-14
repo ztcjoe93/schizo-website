7ca5f0a6d4f702e0052a77b4b5943946e7d5d14b
zt <ztcjoe93@gmail.com>
2021-05-15 00:55:33 +0800

    Updates

---
+++ precache-manifest.a45f16a07201f38a2af7ea941120b643.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a169626898ac98117387dbfb1e562490",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d73e3b4d632b42d81790",
+    "url": "/static/css/main.45adb9b2.chunk.css"
+  },
+  {
+    "revision": "707a89da7e660803f687",
+    "url": "/static/js/2.b9f9632c.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.b9f9632c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d73e3b4d632b42d81790",
+    "url": "/static/js/main.f9f7fc45.chunk.js"
+  },
+  {
+    "revision": "c307e69827cf23a9bfac",
+    "url": "/static/js/runtime-main.b8b9f84b.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "d985e4c92be5d39c6702979c2f4c66e7",
+    "url": "/static/media/display1.d985e4c9.png"
+  },
+  {
+    "revision": "5fe17d3256cbc6f220f374f4f163394a",
+    "url": "/static/media/display2.5fe17d32.png"
+  },
+  {
+    "revision": "31b3b2fb8f2ff9fabdef10544805a674",
+    "url": "/static/media/display3.31b3b2fb.png"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "ed46c32d0003eafc8e04e1bbd53783d2",
+    "url": "/static/media/illustration-section-01.ed46c32d.svg"
+  },
+  {
+    "revision": "cfe34f516bdc37c53550fd030c449f55",
+    "url": "/static/media/illustration-section-02.cfe34f51.svg"
+  },
+  {
+    "revision": "a2f0348107933424ea07c0a36edb222d",
+    "url": "/static/media/puzzle.a2f03481.svg"
+  },
+  {
+    "revision": "acfb8ae20317b6918f7609a947d0c93e",
+    "url": "/static/media/studiologo_black.acfb8ae2.jpeg"
+  },
+  {
+    "revision": "84d4071bf6d000b7029df422dec6dd98",
+    "url": "/static/media/trailer-placeholder.84d4071b.png"
+  },
+  {
+    "revision": "06ebb184622f1d45e9309ad13724c462",
+    "url": "/static/media/trailer.06ebb184.mp4"
+  },
+  {
+    "revision": "b36a5d3bc01bc3de7f94dc906542e324",
+    "url": "/static/media/unreal-engine.b36a5d3b.svg"
+  }
+]);
